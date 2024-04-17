9780eeb65b99c33f67a6478551f83a2dad06d4e8
Danh Phan <phandanh2112@gmail.com>
2024-04-17 23:21:53 +0700

    Led_Reg - ver 1.2: blink led using counter up timer

--- Led_Reg/Debug/Src/main.d
+++ Led_Reg/Debug/Src/main.d
@@ -5,7 +5,8 @@ Src/main.o: ../Src/main.c \
  D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Include/cmsis_compiler.h \
  D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Include/cmsis_gcc.h \
  D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Include/mpu_armv7.h \
- D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+ D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/Timer.h ../Inc/SystemClock.h
 D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
 D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Include/core_cm4.h:
 D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Include/cmsis_version.h:
@@ -13,3 +14,5 @@ D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Include/cmsis_compiler.
 D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Include/cmsis_gcc.h:
 D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Include/mpu_armv7.h:
 D:/cd/STM32/Project/STM32_Projects/Led_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/Timer.h:
+../Inc/SystemClock.h:
