df9b0ce4dee9b82573eecdfd8291e8974faa7920
Danh Phan <phandanh2112@gmail.com>
2024-04-29 23:00:38 +0700

    UART_Reg - ver 1.0: transmit data

---
+++ UART_Reg/Inc/USART.h
@@ -0,0 +1,54 @@
+/*
+ * USART.h
+ *
+ *  Created on: Apr 29, 2024
+ *      Author: Danh21
+ */
+
+#ifndef USART_H_
+#define USART_H_
+
+#define f_PCLK 		16000000
+#define baudrate 	115200
+
+
+
+/* Initializing UART */
+void USART_init(USART_TypeDef *USART)
+{
+	uint32_t USART_DIV, DIV_Mantissa, DIV_Fraction;
+
+	if (USART == USART2)
+	{
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;		// IO port A clock enable
+
+		GPIOA->MODER |= GPIO_MODER_MODER2_1;		// alternate function for PA2
+		GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED2_1;	// high speed for PA2
+		GPIOA->AFR[0] |= (7<<8);					// USART2 mode for PA2
+
+		RCC->APB1ENR |= RCC_APB1ENR_USART2EN; 		// USART2 clock enable
+	}
+
+	USART->CR1 |= USART_CR1_TE; 					// transmit enable
+	USART->CR1 |= USART_CR1_UE;						// USART enable
+
+	USART_DIV = f_PCLK / (baudrate * 16);
+	DIV_Mantissa = USART_DIV;
+	DIV_Fraction = (USART_DIV - DIV_Mantissa) * 16;
+	USART->BRR |= (DIV_Mantissa<<4) | (DIV_Fraction);	// set baudrate
+}
+
+
+
+/* transmit data */
+void USART_transmit(USART_TypeDef *USART, char *p)
+{
+	while (*p != '\0')
+	{
+		USART->DR = *p;											// load data
+		while ((USART->SR & USART_SR_TC_Msk) != USART_SR_TC);	// wait until Transmission is complete
+		p++;
+	}
+}
+
+#endif /* USART_H_ */
---
+++ UART_Reg/Src/main.c
@@ -0,0 +1,53 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Danh21
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ *							REVISION HISTORY
+ * Version 1.0: transmit data by UART
+ ******************************************************************************
+ */
+/* LIBRARIES */
+#include <stdint.h>
+#include <stm32f407xx.h>
+#include "Timer.h"
+#include "SystemClock.h"
+#include "USART.h"
+
+
+
+/* VARS */
+USART_TypeDef *USART = USART2;
+TIM_TypeDef *TIM = TIM1;
+
+
+
+/* MACROES */
+#define send_data(x) 	USART_transmit(USART, x);
+#define delay_ms(x)		Timer_delay_ms(TIM, x)
+
+
+
+/* MAIN */
+int main()
+{
+	SystemClock_config();
+	Timer_init(TIM);
+	USART_init(USART);
+
+	while(1)
+	{
+		send_data("hello world\n");
+		delay_ms(1000);
+	}
+}
