1553df71a80d765590a12fac5fb3978ce9a78c41
Danh Phan <phandanh2112@gmail.com>
2024-08-26 22:30:18 +0700

    Led_Reg - ver 3.0: blink led by timer; apply Led.h; modify Timer.h and SystemClock.h

--- Led_Reg/Inc/Timer.h
+++ Led_Reg/Inc/Timer.h
@@ -1,41 +1,78 @@
 /*
  * Timer.h
  *
- *  Created on: Apr 17, 2024
- *      Author: DELL
+ * Created on: Apr 17, 2024
+ * Author: Danh21
+ *
+ *      			REVISION HISTORY
+ * Version 1.0: Init and create delay by TIM1
+ * Version 2.0: Init and create delay by any TIM
  */
+#include "SystemClock.h"
 
 #ifndef TIMER_H_
 #define TIMER_H_
 
 
 
-const int f_PCLK = 16000000;
-
-#define T_timer		0.001		// ms
+/* MACRO */
+#define T_timer		0.001		// expect 1ms
 #define AutoReload	65535		// Auto-reload value
 
 
 
 /*
- * Initialize Timer 1
+ * Initialize Timer
  * */
-void Timer1_init()
+void Timer_init(TIM_TypeDef *TIM)
 {
-	uint32_t F_timer, prescaler;
+	uint32_t F_timer, prescaler, f_CLK;
 
-	// F_timer = f_PCLK / (PSC + 1)
+	// get frequency of CLK
+	if (TIM == TIM1 || TIM == TIM8 || TIM == TIM9 || TIM == TIM10 || TIM == TIM11)
+		f_CLK = Get_PCLK2_freq();
+	else if (TIM == TIM2 || TIM == TIM3 || TIM == TIM4 || TIM == TIM5 || TIM == TIM6 || TIM == TIM7 || TIM == TIM12 || TIM == TIM13 || TIM == TIM14)
+		f_CLK = Get_PCLK1_freq();
+
+	// TIM clock enable
+	if (TIM == TIM1)
+		RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+	else if (TIM == TIM8)
+		RCC->APB2ENR |= RCC_APB2ENR_TIM8EN;
+	else if (TIM == TIM9)
+		RCC->APB2ENR |= RCC_APB2ENR_TIM9EN;
+	else if (TIM == TIM10)
+		RCC->APB2ENR |= RCC_APB2ENR_TIM10EN;
+	else if (TIM == TIM11)
+		RCC->APB2ENR |= RCC_APB2ENR_TIM11EN;
+	else if (TIM == TIM2)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	else if (TIM == TIM3)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+	else if (TIM == TIM4)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+	else if (TIM == TIM5)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM5EN;
+	else if (TIM == TIM6)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+	else if (TIM == TIM7)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM7EN;
+	else if (TIM == TIM12)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM12EN;
+	else if (TIM == TIM13)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM13EN;
+	else if (TIM == TIM14)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM14EN;
+
+	// F_timer = f_CLK / (PSC + 1)
 	F_timer = 1 / T_timer;
-	prescaler = f_PCLK / F_timer - 1;
+	prescaler = f_CLK / F_timer - 1;
 
-	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN; 		// TIM1 clock enable
+	TIM->PSC |= prescaler;
+	TIM->ARR = AutoReload;
 
-	TIM1->PSC |= prescaler;
-
-	TIM1->ARR = AutoReload;
-
-	TIM1->CR1 |= TIM_CR1_CEN;					// Counter enable
-	TIM1->CR1 &= ~(TIM_CR1_DIR);				// Counter used as up counter
+	TIM->CR1 |= TIM_CR1_CEN;		// Counter enable
+	TIM->CR1 &= ~(TIM_CR1_DIR);		// Counter used as up counter
 }
 
 
@@ -43,10 +80,10 @@ void Timer1_init()
 /*
  * delay by ms
  * */
-void Timer1_delay_ms(int ms)
+void Timer_delay_ms(TIM_TypeDef *TIM, int ms)
 {
-	TIM1->CNT = 0;
-	while (TIM1->CNT < ms);
+	TIM->CNT = 0;
+	while (TIM->CNT < ms);
 }
 
 
--- Led_Reg/Src/main.c
+++ Led_Reg/Src/main.c
@@ -19,26 +19,23 @@
  * Version 1.2: blink led by periodically using counter up timer
  *
  * Version 2.0: toggle led by button using polling technique
+ *
+ * Version 3.0: blink led by periodically using counter up timer; apply Led.h lib
  ******************************************************************************
  */
 /* LIBRARIES */
 #include <stdint.h>
 #include <stm32f407xx.h>
-#include "Timer.h"
 #include "SystemClock.h"
+#include "Timer.h"
+#include "Led.h"
 
 
 
-/* MACROES */
-#define led_on 		GPIOD->ODR |= 1<<13
-#define led_off		GPIOD->ODR &= ~(1<<13)
-#define led_is_off	(GPIOD->ODR & 1<<13) == 0
-#define toggle_led 	led_is_off ? led_on : (led_off)
-
-
-
-/* PROTOTYPE */
-void GPIO_init(void);
+/* CONFIG */
+TIM_TypeDef *TIM = TIM8;
+user_led led = LD3;
+uint32_t your_delay = 1000;
 
 
 
@@ -46,21 +43,12 @@ void GPIO_init(void);
 int main()
 {
 	SystemClock_config();
-	GPIO_init();
-	Timer1_init();
+	LED_init(led);
+	Timer_init(TIM);
 
 	while(1)
 	{
-		toggle_led;
-		Timer1_delay_ms(500);
+		toggle_led(led);
+		Timer_delay_ms(TIM, your_delay);
 	}
-}
-
-
-
-/* APIs */
-void GPIO_init(void)
-{
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;	// IO port D clock enable
-	GPIOD->MODER |= GPIO_MODER_MODER13_0;	// General purpose output mode
 }
